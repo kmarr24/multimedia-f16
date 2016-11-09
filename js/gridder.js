92a0d25258aaeb35483273eb81e8b1509c064fbe
keith <kpm67@cabrini.edu>
2016-11-09 11:42:00 -0500

    initial commit

---
+++ js/gridder.js
@@ -0,0 +1,8 @@
+// Loads the JQuery Library //
+$(document).ready(function() {
+createGridder = function() {
+  document.body.appendChild(
+    document.createElement('script'))
+    .src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/788826/960.gridder.js';
+}
+  }); // End of ready function.
